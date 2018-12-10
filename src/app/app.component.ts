e546c10adacd3cb026f84804b112f78d3a2f26c0
LookItsBlu <anthony-62100@hotmail.fr>
2018-12-10 19:42:19 +0100

    ✨ Added drag and drop table part 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DragulaService } from 'ng2-dragula';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'dragula';
+export class AppComponent implements OnInit {
+  public items: string[][];
+
+  constructor(
+    private ds: DragulaService
+  ) {
+    this.items = [];
+    for (let x = 0; x < 100; x += 1) {
+      this.items.push([]);
+      for (let y = 0; y < 100; y += 1) {
+        this.items[x].push(`${x}, ${y}`);
+      }
+    }
+  }
+
+  ngOnInit(): void {
+    this.ds.dropModel('LIST').subscribe((val) => console.log('item', val));
+  }
 }
